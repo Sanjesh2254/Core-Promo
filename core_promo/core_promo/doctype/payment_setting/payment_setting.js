3762f2a2dcdc116ead0db9f1a1e30515914c54f7
Sanjesh <rsanjesh64@gmail.com>
2026-03-08 14:57:43 +0530

    feat:Added Task work log

---
+++ core_promo/core_promo/doctype/payment_setting/payment_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, core promo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Payment Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ core_promo/core_promo/doctype/task_work_log/task_work_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, core promo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Task Work Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
